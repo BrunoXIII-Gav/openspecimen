d6f706d2ea8232f363505665f461477d5c7901ec
ArnySalazar <xxteam24xx@gmail.com>
2026-06-24 00:41:15 -0500

    feat: add labelCode i18n support for 470 permissible values across 16 categories

--- ui/src/administrative/schemas/pvs/values.js
+++ ui/src/administrative/schemas/pvs/values.js
@@ -1,11 +1,16 @@
 
 import routerSvc from '@/common/services/Router.js';
+import i18n      from '@/common/services/I18n.js';
 
 export default {
   columns: [
     {
       name: 'pv.value',
       captionCode: 'pvs.value',
+      value: (rowObject) => {
+        const pv = rowObject.pv;
+        return i18n.exists(pv.labelCode) ? i18n.msg(pv.labelCode) : pv.value;
+      },
       href: (row) => routerSvc.getUrl('PvAddEdit', {pvId: row.rowObject.pv.id}, {attribute: row.rowObject.pv.attribute})
     },
     {
